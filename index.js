e285d9669253542f8b05ebfee79d550d7bff562f
GouthamGB <gouthamb2000@gmail.com>
2023-03-20 12:56:12 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.get("/drug1", (req, res) => {
+  fs.readFile("database/drug1.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Server error" });
+    }
+
+    res.json(JSON.parse(data));
+  });
+});
+
+app.get("/drug2", (req, res) => {
+    fs.readFile("database/drug2.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Server error" });
+      }
+  
+      res.json(JSON.parse(data));
+    });
+  });
+
+
+app.listen(5000, () => {
+  console.log("Server started on port 3000");
+});
